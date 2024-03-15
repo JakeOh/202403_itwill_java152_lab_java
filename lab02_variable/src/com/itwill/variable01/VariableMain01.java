e6a67920aa66a3643682401099224dc481f35cc9
Jake Oh <jake.oh.lecture@gmail.com>
2024-03-15 10:26:46 +0900

    variable

---
+++ lab02_variable/src/com/itwill/variable01/VariableMain01.java
@@ -0,0 +1,17 @@
+package com.itwill.variable01;
+
+public class VariableMain01 {
+
+    public static void main(String[] args) {
+        // 변수(variable): 프로그램에서 필요한 데이터를 저장하는 메모리 공간.
+        // 변수 사용: 
+        // (1) 변수 선언: 저장하는 데이터의 종류(타입)과 변수 이름을 선언.
+        // (2) 변수 초기화: 변수에 값을 처음 저장하는 것.
+        int age; // 정수(integer)를 저장하는 변수 age
+        age = 16; // 변수 age를 16으로 초기화(변수 age에 정수 16을 저장)
+        
+        System.out.println(age);
+
+    }
+
+}
