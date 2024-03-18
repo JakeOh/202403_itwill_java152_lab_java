3a2a2f0ef0e1469d3f0083f704db1fb12675245b
Jake Oh <jake.oh.lecture@gmail.com>
2024-03-18 17:28:32 +0900

    homework

---
+++ lab03_control/src/com/itwill/rsp/Rsp.java
@@ -0,0 +1,26 @@
+package com.itwill.rsp;
+
+public class Rsp {
+
+    public static void main(String[] args) {
+        // 가위/바위/보 (Rock-Scissors-Paper) 게임:
+        // 가위 - 0, 바위 - 1, 보 - 2
+        
+        // TODO: Random 타입 변수를 선언, 초기화.
+        // TODO: Scanner 타입 변수를 선언, 초기화.
+
+        System.out.println("가위바위보 게임");
+        System.out.println("---------------");
+        System.out.println("[0] 가위");
+        System.out.println("[1] 바위");
+        System.out.println("[2] 보");
+        System.out.println("---------------");
+        System.out.print("선택>>> ");
+        
+        // TODO: 사용자가 콘솔창에서 입력한 정수를 변수(user)에 저장.
+        // TODO: 1 이상 4 미만의 정수 난수 1개를 변수(computer)에 저장.
+        // TODO: 가위바위보 게임 결과(User win/Computer win/Tie)를 출력.
+        
+    }
+
+}
