d30c8b5be18bcfd1fe16f81d5e147efce30257b4
Jake Oh <jake.oh.lecture@gmail.com>
2024-03-19 17:35:23 +0900

    homework

---
+++ lab03_control/src/com/itwill/loop07/LoopMain07.java
@@ -0,0 +1,13 @@
+package com.itwill.loop07;
+
+public class LoopMain07 {
+
+    public static void main(String[] args) {
+        // 과제: 구구단 출력하기
+        // 2단은 2x2까지만, 3단은 3x3까지만, 4단은 4x4까지만, ..., 9단은 9x9까지 출력.
+        // (1) break 이용하기.
+        // (2) break 이용하지 않기.
+
+    }
+
+}
