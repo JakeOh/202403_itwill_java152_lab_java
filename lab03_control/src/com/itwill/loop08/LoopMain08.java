dae22191b720fe14de2e6db494da793512241f9d
Jake Oh <jake.oh.lecture@gmail.com>
2024-03-20 11:18:05 +0900

    while

--- lab03_control/src/com/itwill/loop08/LoopMain08.java
+++ lab03_control/src/com/itwill/loop08/LoopMain08.java
@@ -12,6 +12,36 @@ public class LoopMain08 {
             n++; // n += 1; n = n + 1;
         }
         System.out.println();
+        
+        System.out.println("----- [2] -----");
+        // 5 4 3 2 1 출력
+        n = 5;
+        while (n > 0) {
+            System.out.print(n + " ");
+            n--;
+        }
+        System.out.println();
+        
+        System.out.println("----- [3] -----");
+        // 1 3 5 7 9 출력
+        n = 1;
+        while (n < 10) {
+            System.out.print(n + " ");
+            n += 2; // n = n + 2;
+        }
+        System.out.println();
+        
+        n = 1;
+        while (n < 10) {
+            if (n % 2 == 1) {
+                System.out.print(n + " ");
+            }
+            n++;
+        }
+        System.out.println();
+        
+        System.out.println("----- [4] -----");
+        // while문을 사용해서 구구단 9단을 출력.
 
     }
 
