737f3c81e4e6f2ef3d5406d88e103b3a32640107
Jake Oh <jake.oh.lecture@gmail.com>
2024-03-20 16:24:51 +0900

    loop exercises

---
+++ lab03_control/src/com/itwill/loop13/LoopMain13.java
@@ -0,0 +1,25 @@
+package com.itwill.loop13;
+
+public class LoopMain13 {
+
+    public static void main(String[] args) {
+        /* 교재 연습문제 5
+           출력 결과
+           *
+           **
+           ***
+           ****
+         */
+        
+        
+        /* 교재 연습문제 6
+           출력 결과
+               *
+              **
+             ***
+            ****
+         */
+        
+    }
+
+}
