bd548c7998b185e2db26c31e9ff137b98d99cbb2
Jake Oh <jake.oh.lecture@gmail.com>
2024-03-26 12:21:14 +0900

    OOP: constructor

--- lab06_class/src/com/itwill/class02/ClassMain02.java
+++ lab06_class/src/com/itwill/class02/ClassMain02.java
@@ -16,6 +16,12 @@ public class ClassMain02 {
         System.out.println("총점: " + score1.getTotal());
         System.out.println("평균: " + score1.getMean());
 
+        // 아규먼트를 갖는 생성자를 이용한 Score 객체 생성:
+        Score score2 = new Score(90, 95, 80);
+        System.out.println("score2 java = " + score2.java);
+        System.out.println("score2 총점: " + score2.getTotal());
+        System.out.println("score2 평균: " + score2.getMean());
+        
     }
 
 }
