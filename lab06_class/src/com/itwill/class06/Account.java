a7122705cf35a667c17c4c4d9e8135de73bc556a
Jake Oh <jake.oh.lecture@gmail.com>
2024-03-27 14:29:34 +0900

    OOP: field, constructor, method

--- lab06_class/src/com/itwill/class06/Account.java
+++ lab06_class/src/com/itwill/class06/Account.java
@@ -11,6 +11,10 @@ public class Account {
     int balance; // 잔고
     
     // constructor: 아규먼트 2개를 갖는 생성자
+    public Account(int accountNo, int balance) {
+        this.accountNo = accountNo;
+        this.balance = balance;
+    }
     
     // method
     /**
@@ -18,12 +22,20 @@ public class Account {
      * @param amount 입금액(int).
      * @return 입금 후 잔고(balance).
      */
+    public int deposit(int amount) {
+        this.balance += amount; // balance = balance + amount;
+        return this.balance;
+    }
     
     /**
      * withdraw(출금). 현재 잔고에서 출금액을 빼고, 출금 후의 잔고를 리턴.
      * @param amount 출금액(int).
      * @return 출금 후 잔고.
      */
+    public int withdraw(int amount) {
+        this.balance -= amount;
+        return this.balance;
+    }
     
     /**
      * transfer(이체). 내 계좌에서의 잔고에서는 이체금액 amount를 빼고, 
@@ -32,6 +44,14 @@ public class Account {
      * @param amount 이체할 금액.
      * @return true.
      */
+    public boolean transfer(Account to, int amount) {
+        //this.balance -= amount; // 내 계좌에서 출금
+        this.withdraw(amount);
+        //to.balance += amount; // 상대방 계좌에 입금
+        to.deposit(amount);
+        
+        return true;
+    }
     
     /**
      * 계좌 정보 출력
