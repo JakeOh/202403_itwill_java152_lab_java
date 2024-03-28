ee5d89f5afa9c75967e7d29e7777296272c29677
Jake Oh <jake.oh.lecture@gmail.com>
2024-03-28 17:18:24 +0900

    contact v0.1

--- lab_contact/src/com/itwill/ver01/ContactMain.java
+++ lab_contact/src/com/itwill/ver01/ContactMain.java
@@ -3,20 +3,55 @@ package com.itwill.ver01;
 import java.util.Scanner;
 
 public class ContactMain {
+    private static final int MAX_LENGTH = 3; // 연락처 배열의 최대 길이(원소 개수)
     
     private Scanner scanner = new Scanner(System.in);
+    private Contact[] contacts = new Contact[MAX_LENGTH]; // 연락처를 저장하기 위한 배열
+    private int count = 0; // 현재까지 연락처 배열에 저장된 개수.
+    // 배열에 새로운 연락처 저장될 때마다 count++을 실행.
 
     public static void main(String[] args) {
         System.out.println("*** 연락처 프로그램 v0.1");
         
+        ContactMain app = new ContactMain();
+        
         // 프로그램 계속 실행(run=true) 또는 종료(run=false) 여부를 저장하기 위한 변수.
         boolean run = true;
         while (run) {
-            // TODO 메인 메뉴 보여주기
-            // switch ()
+            int menu = app.showMainMenu();
+            switch (menu) {
+            case 0:
+                run = false;
+                break;
+            case 1:
+                app.saveNewContact();
+                break;
+            default:
+                System.out.println("메뉴 번호(0 ~ 4)를 확인하세요.");
+            }
         }
         
         System.out.println("*** 프로그램 종료 ***");
+    }
+    
+    private void saveNewContact() {
+        System.out.println("\n--- 새 연락처 저장 ---");
+        
+        System.out.print("이름 입력>> ");
+        String name = scanner.nextLine();
+        
+        System.out.print("전화번호 입력>> ");
+        String phone = scanner.nextLine();
+        
+        System.out.print("이메일 입력>> ");
+        String email = scanner.nextLine();
+        
+        Contact contact = new Contact(name, phone, email);
+        
+        // Contact 타입 객체를 연락처 배열 인덱스 count에 저장
+        contacts[count] = contact;
+        // 배열에 저장 후에는 연락처 저장 개수(인덱스)를 증가
+        count++;
     }
     
     private int showMainMenu() {
