e6e0e31f69f3837d18e774e0366f2863391e050d
Jake Oh <jake.oh.lecture@gmail.com>
2024-03-29 18:23:51 +0900

    inheritance & constructor

---
+++ lab07_inheritance/src/com/itwill/inheritance02/BusinessPerson.java
@@ -0,0 +1,34 @@
+package com.itwill.inheritance02;
+
+// 회사원은 사람이다. (BusinessPerson IS A Person.)
+public class BusinessPerson extends Person {
+    // 필드
+    private String company;
+    
+    // 생성자
+    public BusinessPerson() {
+        System.out.println("BusinessPerson() 생성자");
+    }
+    
+    public BusinessPerson(String company) {
+//        super();
+        this.company = company;
+        System.out.println("BusinessPerson(company) 생성자");
+    }
+    
+    public BusinessPerson(String name, String company) {
+        super(name);
+        this.company = company;
+        System.out.println("BusinessPerson(name, company) 생성자");
+    }
+    
+    // 메서드
+    public String getCompany() {
+        return this.company;
+    }
+    
+    public void setCompany(String company) {
+        this.company = company;
+    }
+    
+}
