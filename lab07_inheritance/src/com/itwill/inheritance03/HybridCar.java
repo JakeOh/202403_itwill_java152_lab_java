d87108fbd75d66165bc686bfee1d6a324109a82b
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-01 12:15:42 +0900

    inheritance & polymorphism

--- lab07_inheritance/src/com/itwill/inheritance03/HybridCar.java
+++ lab07_inheritance/src/com/itwill/inheritance03/HybridCar.java
@@ -18,7 +18,19 @@ public class HybridCar extends Car {
         System.out.println("충전중: battery=" + battery);
     }
     
+    /*
+     * 메서드/생성자 오버로딩(overloading):
+     * 파라미터가 다른 같은 이름의 메서드/생성자를 여러개 선언(정의).
+     * 
+     * 메서드 오러라이딩(overriding):
+     * 상위 타입에서 선언된 메서드를 하위 타입에서 재정의하는 것.
+     * 리턴 타입, 메서드 이름, 파라미터 선언이 모두 같아야 함.
+     * 접근 수식어는 상위 타입의 가시성 범위와 같거나 더 넓어질 수 있음.
+     */
+    
+    @Override // 애너테이션(annotation) - 자바 컴파일러가 사용.
     public void drive() {
+        // super.drive(); //-> 상위 객체의 재정의 하기 전의 메서드 호출
         System.out.println("하이브리드자동차 운전중(속력: " + getSpeed()
                 + ", 연료: " + getFuel()
                 + ", 배터리: " + battery
--- lab07_inheritance/src/com/itwill/inheritance03/InheritanceMain03.java
+++ lab07_inheritance/src/com/itwill/inheritance03/InheritanceMain03.java
@@ -10,6 +10,13 @@ public class InheritanceMain03 {
         // HybridCar 타입의 객체를 생성:
         HybridCar car2 = new HybridCar(25, 30, 0);
         car2.drive();
+        
+        // 다형성(polymorphism)을 사용한 변수 선언과 초기화:
+        // SuperType var = new SubType();
+        Car car3 = new HybridCar(50, 40, 100);
+
+        // SubType var = new SuperType(); -> 컴파일 오류
+        // HybridCar car = new Car(100, 100);
 
     }
 
