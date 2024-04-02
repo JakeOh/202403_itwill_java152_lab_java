8b79436c74fcdeb7b48e0e11030b56bf70ca8423
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-02 14:23:10 +0900

    abstract class & methods

---
+++ lab07_inheritance/src/com/itwill/inheritance08/Circle.java
@@ -0,0 +1,9 @@
+package com.itwill.inheritance08;
+
+public class Circle extends Shape {
+    // field
+    private double radius;
+    
+    
+
+}
---
+++ lab07_inheritance/src/com/itwill/inheritance08/InheritanceMain08.java
@@ -0,0 +1,12 @@
+package com.itwill.inheritance08;
+
+public class InheritanceMain08 {
+
+    public static void main(String[] args) {
+        // Rectangle 타입의 객체 생성
+        Rectangle rect = new Rectangle(4.0, 3.0);
+        rect.draw();
+
+    }
+
+}
