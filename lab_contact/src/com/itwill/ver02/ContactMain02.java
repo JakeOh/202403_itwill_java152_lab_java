e9b5dd242d90a7b229378d224287c3817d21005c
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-04 17:23:10 +0900

    contact v0.2 - mvc & exception

---
+++ lab_contact/src/com/itwill/ver02/ContactMain02.java
@@ -0,0 +1,12 @@
+package com.itwill.ver02;
+
+// MVC 아키텍쳐에서 View 역할.
+public class ContactMain02 {
+
+    public static void main(String[] args) {
+        System.out.println("*** 연락처 프로그램 v0.2 ***");
+        
+        System.out.println(">>> 프로그램 종료 >>>");
+    }
+
+}
