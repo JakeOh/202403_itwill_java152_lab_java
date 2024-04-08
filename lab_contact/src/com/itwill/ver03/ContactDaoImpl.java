5ae974a71d2390c7591c8e3c3e31b9f8902f7d51
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-08 16:58:28 +0900

    contact v0.3

---
+++ lab_contact/src/com/itwill/ver03/ContactDaoImpl.java
@@ -0,0 +1,39 @@
+package com.itwill.ver03;
+
+import java.util.List;
+
+import com.itwill.ver01.Contact;
+
+public class ContactDaoImpl implements ContactDao {
+
+    @Override
+    public int create(Contact contact) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<Contact> read() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Contact read(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int update(int index, Contact contact) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int delete(int index) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
