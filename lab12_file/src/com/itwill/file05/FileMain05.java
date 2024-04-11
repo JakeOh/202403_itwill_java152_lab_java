2d8ac75de90d4f641bcfbd37df0f924a53a95a15
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-11 09:23:45 +0900

    Object Input/Output Stream, Buffered Input/Output Stream

--- lab12_file/src/com/itwill/file05/FileMain05.java
+++ lab12_file/src/com/itwill/file05/FileMain05.java
@@ -1,5 +1,11 @@
 package com.itwill.file05;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import com.itwill.file04.Product;
@@ -19,9 +25,44 @@ public class FileMain05 {
         
         // 1. Product 타입 객체 1,000,000개를 저장하는 ArrayList를 파일 쓰기
         // FOS, BOS, OOS을 이용.
+        try (
+                FileOutputStream out = new FileOutputStream(fileName);
+                BufferedOutputStream bos = new BufferedOutputStream(out);
+                ObjectOutputStream oos = new ObjectOutputStream(bos);
+        ) {
+            long start = System.currentTimeMillis();
+            
+            oos.writeObject(list);
+            
+            long end = System.currentTimeMillis();
+            
+            System.out.println("파일 쓰기 시간: " + (end - start) + "ms");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
         // 2. 파일에서 객체를 읽어서 ArrayList<Product>로 변환하기
         // FIS, BIS, OIS을 이용.
+        try (
+                FileInputStream in = new FileInputStream(fileName);
+                BufferedInputStream bis = new BufferedInputStream(in);
+                ObjectInputStream ois = new ObjectInputStream(bis);
+        ) {
+            long start = System.currentTimeMillis();
+            
+            ArrayList<Product> products = (ArrayList<Product>) ois.readObject();
+            
+            long end = System.currentTimeMillis();
+            
+            System.out.println("파일 읽기 시간: " + (end - start) + "ms");
+            System.out.println("size = " + products.size());
+            System.out.println(products.getFirst());
+            System.out.println(products.getLast());
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
