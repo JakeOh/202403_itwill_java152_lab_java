d5dd9299cf314100f944cf0fc9bc6a5ec509c8af
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-11 16:52:10 +0900

    contact v0.4

--- lab_contact/src/com/itwill/ver04/view/ContactMain04.java
+++ lab_contact/src/com/itwill/ver04/view/ContactMain04.java
@@ -1,7 +1,10 @@
 package com.itwill.ver04.view;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.itwill.ver04.model.Contact;
 import com.itwill.ver04.util.FileUtil;
 
 public class ContactMain04 {
@@ -10,6 +13,14 @@ public class ContactMain04 {
         // FileUtil 테스트
         File f = FileUtil.initializeDataDir();
         System.out.println(f);
+        
+//        FileUtil.readDataFromFile();
+        List<Contact> list = FileUtil.initializeData();
+        System.out.println(list);
+        
+        List<Contact> data = new ArrayList<Contact>();
+        data.add(new Contact());
+        FileUtil.writeDataToFile(data);
 
     }
 
