c0fcaa609a39944e9e255e5a6b18c047eb109953
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-15 17:27:15 +0900

    Thread, Runnable

--- lab14_thread/src/com/itwill/thread01/ThreadMain01.java
+++ lab14_thread/src/com/itwill/thread01/ThreadMain01.java
@@ -22,13 +22,13 @@ public class ThreadMain01 {
                 this.name = name;
             }
             
-            // (2) run 메서드 재정의
+            // (2) run 메서드 재정의 -> 쓰레드가 할 일
             @Override
             public void run() {
                 for (int i = 0; i < 100; i++) {
                     System.out.println(i + " : " + name);
                     try {
-                        sleep(100); // Thread 클래스의 static 메서드 호출
+                        sleep(100); // Thread 클래스의 static 메서드 호출 - 0.1초(100ms = 100/1000초) 쉼.
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -38,10 +38,29 @@ public class ThreadMain01 {
         
         // (3) MyThread 객체 생성
         MyThread th1 = new MyThread("쓰레드");
+        MyThread th2 = new MyThread("Hello");
+        MyThread th3 = new MyThread("안녕하세요");
+        
+        long start = System.currentTimeMillis(); // 쓰레드 시작 시간.
         
         // (4) MyThread 객체의 start() 메서드 호출
         th1.start();
+        th2.start();
+        th3.start();
 
+        // main 프로세스가 실행시킨 다른 프로세스(쓰레드)들이 모두 종료될 때까지 기다림.
+        try {
+            th1.join();
+            th2.join();
+            th3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        // th1, th2 모두 종료된 후 시간
+        long end = System.currentTimeMillis();
+        System.out.println("경과 시간: " + (end - start) + "ms");
+        
         System.out.println("***** main 메서드 종료 *****");
     }
 
