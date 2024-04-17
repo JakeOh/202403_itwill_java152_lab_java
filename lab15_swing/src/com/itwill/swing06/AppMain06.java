89c01fef03478661c2ec911accec1bcb66661c61
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-17 12:13:44 +0900

    JRadioButton, JCheckBox

--- lab15_swing/src/com/itwill/swing06/AppMain06.java
+++ lab15_swing/src/com/itwill/swing06/AppMain06.java
@@ -12,6 +12,8 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AppMain06 {
 
@@ -62,6 +64,12 @@ public class AppMain06 {
         frame.getContentPane().setLayout(null);
         
         rbPrivate = new JRadioButton("private");
+        rbPrivate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleRadioButtonClick(e);
+            }
+        });
         rbPrivate.setSelected(true);
         buttonGroup.add(rbPrivate);
         rbPrivate.setFont(new Font("D2Coding", Font.PLAIN, 32));
@@ -69,34 +77,51 @@ public class AppMain06 {
         frame.getContentPane().add(rbPrivate);
         
         rbPackage = new JRadioButton("package");
+        rbPackage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleRadioButtonClick(e);
+            }
+        });
         buttonGroup.add(rbPackage);
         rbPackage.setFont(new Font("D2Coding", Font.PLAIN, 32));
         rbPackage.setBounds(184, 6, 172, 64);
         frame.getContentPane().add(rbPackage);
         
         rbProtected = new JRadioButton("protected");
+        rbProtected.addActionListener((e) -> handleRadioButtonClick(e));
         buttonGroup.add(rbProtected);
         rbProtected.setFont(new Font("D2Coding", Font.PLAIN, 32));
         rbProtected.setBounds(363, 6, 172, 64);
         frame.getContentPane().add(rbProtected);
         
         rbPublic = new JRadioButton("public");
+        rbPublic.addActionListener(this::handleRadioButtonClick);
+        // AppMain06.this::handleRadionButtonClick
         buttonGroup.add(rbPublic);
         rbPublic.setFont(new Font("D2Coding", Font.PLAIN, 32));
         rbPublic.setBounds(539, 6, 172, 64);
         frame.getContentPane().add(rbPublic);
         
         cbAbstract = new JCheckBox("abstract");
+        cbAbstract.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleCheckBoxClick(e);
+            }
+        });
         cbAbstract.setFont(new Font("D2Coding", Font.PLAIN, 32));
         cbAbstract.setBounds(8, 85, 172, 64);
         frame.getContentPane().add(cbAbstract);
         
         cbFinal = new JCheckBox("final");
+        cbFinal.addActionListener((e) -> handleCheckBoxClick(e));
         cbFinal.setFont(new Font("D2Coding", Font.PLAIN, 32));
         cbFinal.setBounds(184, 85, 172, 64);
         frame.getContentPane().add(cbFinal);
         
         cbStatic = new JCheckBox("static");
+        cbStatic.addActionListener(this::handleCheckBoxClick);
         cbStatic.setFont(new Font("D2Coding", Font.PLAIN, 32));
         cbStatic.setBounds(363, 85, 172, 64);
         frame.getContentPane().add(cbStatic);
@@ -121,5 +146,21 @@ public class AppMain06 {
         textArea = new JTextArea();
         textArea.setFont(new Font("D2Coding", Font.PLAIN, 32));
         scrollPane.setViewportView(textArea);
+    }
+    
+    private void handleCheckBoxClick(ActionEvent event) {
+        // 3개의 체크박스들 중에서 누가 클릭됐는 지
+        JCheckBox cb = (JCheckBox) event.getSource();
+        String text = cb.getText();
+        boolean selected = cb.isSelected();
+        textArea.setText(text + ": " + selected);
+    }
+
+    private void handleRadioButtonClick(ActionEvent event) {
+        // 4개의 라디오버튼들 중에서 누가 클릭됐는 지 찾기:
+        JRadioButton rb = (JRadioButton) event.getSource();
+        String text = rb.getText(); // 이벤트가 발생한 라디오버튼의 텍스트
+        boolean selected = rb.isSelected(); // 이벤트가 발생한 라디오버튼의 선택 여부
+        textArea.setText(text + ": " + selected);
     }
 }
