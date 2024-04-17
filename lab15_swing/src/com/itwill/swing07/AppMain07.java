50fd80cada76ed9c2e98ff44b7916d8999585da0
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-17 18:17:32 +0900

    homework

--- lab15_swing/src/com/itwill/swing07/AppMain07.java
+++ lab15_swing/src/com/itwill/swing07/AppMain07.java
@@ -123,6 +123,12 @@ public class AppMain07 {
         frame.getContentPane().add(btnCustomDlg);
         
         btnMyFrame = new JButton("Custom Frame");
+        btnMyFrame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO JFrame을 상속받는 객체 보여주기
+            }
+        });
         btnMyFrame.setFont(new Font("D2Coding", Font.BOLD, 32));
         btnMyFrame.setBounds(12, 301, 410, 64);
         frame.getContentPane().add(btnMyFrame);
