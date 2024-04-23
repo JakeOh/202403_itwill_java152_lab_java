72fc6fe13f94284bc7139e8ef89b81ca5135de26
JakeOh <jake.oh.lecture@gmail.com>
2024-04-23 16:27:12 +0900

    subquery

--- lab_sql/sql05.sql
+++ lab_sql/sql05.sql
@@ -38,14 +38,55 @@ where sal > (
 -- Ex. WARD의 연봉보다 더 많은 연봉을 받는 직원들의 이름, 급여, 수당, 연봉을 검색.
 -- 연봉 = sal * 12 + comm. comm(수당)이 null인 경우에는 0으로 계산.
 -- 연봉 내림차순 정렬.
+select ename, sal, comm, sal * 12 + nvl(comm, 0) as ANNUAL_SAL
+from emp
+where sal * 12 + nvl(comm, 0) > (
+    select sal * 12 + nvl(comm, 0) from emp
+    where ename = 'WARD'
+)
+order by ANNUAL_SAL desc;
 
 -- Ex. SCOTT과 같은 급여를  받는 직원들의 이름과 급여를 검색.
+select ename, sal
+from emp
+where sal = (
+    select sal from emp where ename = 'SCOTT'
+);
+
 -- Ex. 위 결과에서 SCOTT은 제외하고 검색.
+select ename, sal
+from emp
+where ename != 'SCOTT'
+    and
+    sal = (
+        select sal from emp where ename = 'SCOTT'
+    );
 
 -- Ex. ALLEN보다 늦게 입사한 직원들의 이름, 입사날짜를 최근입사일부터 출력.
+select ename, to_char(hiredate, 'YYYY/MM/DD') as HIREDATE
+from emp
+where hiredate > (
+    select hiredate from emp where ename = 'ALLEN'
+)
+order by HIREDATE desc;
 
 -- Ex. 매니저가 KING인 직원들의 사번, 이름, 매니저 사번을 검색.
+select empno, ename, mgr
+from emp
+where mgr = (
+    select empno from emp where ename = 'KING'
+);
 
 -- Ex. ACCOUNTING 부서에 일하는 직원들의 이름, 급여, 부서번호를 검색.
+select ename, sal, deptno
+from emp
+where deptno = (
+    select deptno from dept where dname = 'ACCOUNTING'
+);
 
 -- Ex. CHICAGO에서 근무하는 직원들의 이름, 급여, 부서 번호를 검색.
+select ename, sal, deptno
+from emp
+where deptno = (
+    select deptno from dept where loc = 'CHICAGO'
+);
