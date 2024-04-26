627ddd79bd20f15a7be1cf7960bbddb6b41a1f3b
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-26 12:22:30 +0900

    OJDBC

---
+++ lab16_jdbc/src/com/itwill/jdbc01/JdbcMain01.java
@@ -0,0 +1,22 @@
+package com.itwill.jdbc01;
+
+/*
+ * JDBC(Java Database Connectivity):
+ * 자바 프로그램에서 데이터베이스를 연결하고, SQL 문장들을 실행해서 그 결과를 처리.
+ * 
+ * 1. Oracle JDBC 라이브러리를 다운로드하고 이클립스 프로젝트의 빌드패스에 추가.
+ *   (1) https://central.sonatype.com (메이븐 중앙 저장소)에서 ojdbc11을 검색
+ *       ---> com.oracle.database.jdbc:ojdbc11:23.3.0.x.x 라이브러리를 다운로드.
+ *       ---> ojdbc11-23.3.0.x.x.jar
+ *   (2) 이클립스 프로젝트에 lib 폴더를 만들고, jar 파일을 복사.
+ *   (3) jar 파일 오른쪽 클릭 --> Build Path --> Add to build path
+ */
+
+public class JdbcMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
