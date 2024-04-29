6bee43700c6ba3ca59bc91e4ad69b4451508bdab
Jake Oh <jake.oh.lecture@gmail.com>
2024-04-29 16:20:28 +0900

    Blog: search by keyword

--- lab16_jdbc/src/com/itwill/jdbc/controller/BlogDao.java
+++ lab16_jdbc/src/com/itwill/jdbc/controller/BlogDao.java
@@ -67,6 +67,22 @@ public class BlogDao {
         closeResources(conn, stmt, null);
     }
     
+    // ResultSet에서 각 컬럼의 값들을 읽어서 Blog 타입 객체를 생성하고 리턴.
+    private Blog makeBlogFromResultSet(ResultSet rs) throws SQLException {
+        int id = rs.getInt(COL_ID);
+        String title = rs.getString(COL_TITLE);
+        String content = rs.getString(COL_CONTENT);
+        String writer = rs.getString(COL_WRITER);
+        LocalDateTime createdTime = rs.getTimestamp(COL_CREATED_TIME)
+                .toLocalDateTime();
+        LocalDateTime modifiedTime = rs.getTimestamp(COL_MODIFIED_TIME)
+                .toLocalDateTime();
+        
+        Blog blog = new Blog(id, title, content, writer, createdTime, modifiedTime);
+        
+        return blog;
+    }
+    
     // read() 메서드에서 사용할 SQL 문장: select * from blogs order by id desc
     private static final String SQL_SELECT_ALL = String.format(
             "select * from %s order by %s desc", 
@@ -93,16 +109,7 @@ public class BlogDao {
             rs = stmt.executeQuery();
             // 결과 처리.
             while (rs.next()) {
-                int id = rs.getInt(COL_ID);
-                String title = rs.getString(COL_TITLE);
-                String content = rs.getString(COL_CONTENT);
-                String writer = rs.getString(COL_WRITER);
-                LocalDateTime createdTime = rs.getTimestamp(COL_CREATED_TIME)
-                        .toLocalDateTime();
-                LocalDateTime modifiedTime = rs.getTimestamp(COL_MODIFIED_TIME)
-                        .toLocalDateTime();
-                
-                Blog blog = new Blog(id, title, content, writer, createdTime, modifiedTime);
+                Blog blog = makeBlogFromResultSet(rs);
                 result.add(blog);
             }
             
@@ -173,6 +180,83 @@ public class BlogDao {
             e.printStackTrace();
         } finally {
             closeResources(conn, stmt);
+        }
+        
+        return result;
+    }
+    
+    // 제목에 검색 키워드가 포함된 검색 결과:
+    // select * from blogs where lower(title) like ? order by id desc
+    private static final String SQL_SELECT_BY_TITLE = String.format(
+            "select * from %s where lower(%s) like ? order by %s desc", 
+            TBL_BLOGS, COL_TITLE, COL_ID);
+    
+    // 내용에 검색 키워드가 포함된 검색 결과:
+    // select * from blogs where lower(content) like ? order by id desc
+    private static final String SQL_SELECT_BY_CONTENT = String.format(
+            "select * from %s where lower(%s) like ? order by %s desc", 
+            TBL_BLOGS, COL_CONTENT, COL_ID);
+    
+    // 제목 또는 내용에 검색 키워드가 포함된 검색 결과:
+    // select * from blogs where lower(title) like ? or lower(content) like ? order by id desc
+    private static final String SQL_SELECT_BY_TITLE_OR_CONTENT = String.format(
+            "select * from %s " +
+            "where lower(%s) like ? or lower(%s) like ? " + 
+            "order by %s desc", 
+            TBL_BLOGS, COL_TITLE, COL_CONTENT, COL_ID);
+    
+    // 작성자에 검색 키워드가 포함된 검색 결과:
+    // select * from blogs where lower(writer) like ? order by id desc
+    private static final String SQL_SELECT_BY_WRITER = String.format(
+            "select * from %s where lower(%s) like ? order by %s desc", 
+            TBL_BLOGS, COL_WRITER, COL_ID);
+    
+    /**
+     * 제목, 내용, 제목 또는 내용, 작성자로 검색하기.
+     * 검색 타입과 검색어를 전달받아서, 해당 SQL 문장을 실행하고 그 결과를 리턴.
+     * @param type 0 - 제목 검색, 1 - 내용 검색, 2 - 제목 또는 내용 검색, 3 - 작성자 검색.
+     * @param keyword 검색 문자열.
+     * @return 검색 결과 리스트. 검색 결과가 없으면 빈 리스트.
+     */
+    public List<Blog> search(int type, String keyword) {
+        List<Blog> result = new ArrayList<>();
+        
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String searchKeyword = "%" + keyword.toLowerCase() + "%"; // like 검색에서 사용할 파라미터
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            switch (type) {
+            case 0: // 제목으로 검색
+                stmt = conn.prepareStatement(SQL_SELECT_BY_TITLE);
+                stmt.setString(1, searchKeyword);
+                break;
+            case 1: // 내용으로 검색
+                stmt = conn.prepareStatement(SQL_SELECT_BY_CONTENT);
+                stmt.setString(1, searchKeyword);
+                break;
+            case 2: // 제목 또는 내용으로 검색
+                stmt = conn.prepareStatement(SQL_SELECT_BY_TITLE_OR_CONTENT);
+                stmt.setString(1, searchKeyword);
+                stmt.setString(2, searchKeyword);
+                break;
+            case 3: // 작성자로 검색하기
+                stmt = conn.prepareStatement(SQL_SELECT_BY_WRITER);
+                stmt.setString(1, searchKeyword);
+                break;
+            }
+            
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                Blog blog = makeBlogFromResultSet(rs);
+                result.add(blog);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeResources(conn, stmt, rs);
         }
         
         return result;
